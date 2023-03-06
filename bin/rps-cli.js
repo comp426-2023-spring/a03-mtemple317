94120e1e6eca528407318bc563b2b7c98751dc7f
Michelle Temple <michelletemple@MacBook-Pro-378.local>
2023-03-06 10:22:17 -0500

    setting foundation/committing changes

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -1,1 +1,20 @@
 #!/usr/bin/env node
+//These are going to be the files that run when you link/install the package and run node-rps and node-rpsls, respectively
+
+import minimist from 'minimist'
+let args = minimist(process.argv.slice(2))
+const empty = Object.keys(args).length ===1;
+if (args.h || empty === true){
+
+console.log(`Usage: node-rps [SHOT] 
+Play Rock Paper Scissors (RPS)
+-h, --help      display this help message and exit
+-r, --rules     display the rules and exit
+Examples:
+  node-rps        Return JSON with single player RPS result.
+                  e.g. {"player":"rock"}
+  node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+                  e.g {"player":"rock","opponent":"scissors","result":"win"}
+`)
+//process.exit(0)
+}
--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -1,1 +1,2 @@
 #!/usr/bin/env node
+/* These are going to be the files that run when you link/install the package and run node-rps and node-rpsls, respectively*/
