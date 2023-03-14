63be2070c0e074b0d8111e9854d854e03558c877
Michelle Temple <michelletemple@macbook-pro-492.wireless-1x.unc.edu>
2023-03-14 16:59:46 -0400

    added rps function to lib, updated command line arguments to rps-cli, updated winLose function in lib file

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -3,9 +3,9 @@
 import {rps} from "../lib/rpsls.js" 
 import minimist from 'minimist'
 let args = minimist(process.argv.slice(2))
-const empty = Object.keys(args).length ===1;
+const empty = Object.keys(args).length = 1;
 const shot = ["rock", "paper", "scissors"];
-if (args.h || empty === true){
+if (args.h || args.help || empty === true){
 
 console.log(`Usage: node-rps [SHOT] 
 Play Rock Paper Scissors (RPS)
@@ -17,9 +17,9 @@ Examples:
   node-rps rock   Return JSON with results for RPS played against a simulated opponent.
                   e.g {"player":"rock","opponent":"scissors","result":"win"}
 `)
-//process.exit(0)
+process.exit(0)
 }
-if (args.r){
+if (args.r || args.rules){
 
     console.log(`Rules for Rock Paper Scissors:
 
@@ -27,9 +27,10 @@ if (args.r){
     - Paper COVERS Rock
     - Rock CRUSHES Scissors
     `)
-    //process.exit(0)
+    process.exit(0)
     }
 if (!args.h && !args.r && (shot != 'rock' || 'paper' || 'scissors')){
+//  if (args ._. length > 1) {
     console.error(`[ARGUMENT] is out of range`);
     console.log (`Usage: node-rps [SHOT] 
     Play Rock Paper Scissors (RPS)
@@ -47,3 +48,10 @@ if (!args.h && !args.r && (shot != 'rock' || 'paper' || 'scissors')){
                       - Paper COVERS Rock
                       - Rock CRUSHES Scissors`);
     process.exit(0)}
+else if (args ._. length > 1){
+  JSON.stringify(rpsls.rps)
+}
+
+
+/*if (empty === false) {rps;
+    process.exit(0)}*/
